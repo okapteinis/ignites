9e280ca92b1fac6b2e089fa88fb2fda818364289
Ojārs Kapteinis <ojarskapteinis@Makbuks.local>
2025-10-07 22:33:50 +0300

    Update ignites_styles_scripts.php

--- inc/ignites_styles_scripts.php
+++ inc/ignites_styles_scripts.php
@@ -14,7 +14,7 @@ function ignites_enqueue_scripts() {
 	wp_enqueue_script('bootstrap', get_template_directory_uri().'/assets/js/bootstrap.min.js', array(), IGNITES_THEME_VERSION, true);
 	wp_enqueue_script( 'ignites-navigation', get_template_directory_uri() .'/assets/js/navigation.js', array(), IGNITES_THEME_VERSION, true );
 	wp_enqueue_script( 'ignites-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), IGNITES_THEME_VERSION, true );
-	wp_enqueue_script( 'ignites-main-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), IGNITES_THEME_VERSION, true );
+	wp_enqueue_script( 'ignites-main-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery', 'bootstrap'), IGNITES_THEME_VERSION, true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
