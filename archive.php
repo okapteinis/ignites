1f001d0f1e1c46269a0bd93237d1b37de0da190e
Ojārs Kapteinis <ojars@kapteinis.lv>
2026-09-04 01:16:34 +0300

    fix(theme): restore archive h1 after site-title correction

--- archive.php
+++ archive.php
@@ -20,6 +20,9 @@ get_header();
 				<div id="primary" class="content-area">
 					<main id="main" class="site-main">
 						<?php if ( have_posts() ) : ?>
+							<header class="page-header screen-reader-text">
+								<?php the_archive_title( '<h1 class="page-title">', '</h1>' ); ?>
+							</header>
 
 							<?php
 							$ignites_child_post_count = 0;
